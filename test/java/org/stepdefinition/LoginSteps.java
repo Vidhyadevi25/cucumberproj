321b054a81d4dfeae8584340118cc2ba4ef45fca
Vidhyadevi25 <vidhyadevikannaian@gmail.com>
2025-07-13 13:59:06 +0530

    logincode

--- test/java/org/stepdefinition/LoginSteps.java
+++ test/java/org/stepdefinition/LoginSteps.java
@@ -1,5 +1,50 @@
 package org.stepdefinition;
 
-public class LoginSteps {
+import java.util.Date;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.utility.BaseClass;
+import org.utility.LoginPage;
+
+public class LoginSteps extends BaseClass {
+
+	@BeforeClass
+	private void beforeClass() {
+		System.out.println("BeforeClass.."+new Date());
+		browserLaunch("chrome");
+		urlLaunch("https://www.facebook.com/");
+		implicitwait(10);
+	
+		
+	}
+	@AfterClass
+	private void afterClass() {
+		 
+	}
+	@BeforeMethod
+	private void beforeMethod() {
+
+	}
+	@AfterMethod
+	private void afterMethod() {
+		quit();
+	}
+	@Test
+	private void test1() {
+		LoginPage l=new LoginPage();
+		Assert.assertTrue(currentUrl().contains("facebook"));
+		
+		sendkeys(l.getUsername(), "vidhya");
+		
+		sendkeys(l.getPassword(), "12345");
+		click(l.getLogin());
+	}
+	
+	
 
 }
